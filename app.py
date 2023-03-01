d646ca4968a7a9131e4f054e3dead0625978b0e4
Shikhar Dhyani <shikhardhyani025@gmail.com>
2023-03-01 21:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ import seaborn as sns
 import altair as alt
 from textblob import TextBlob
 
+
+from urlextract import URLExtract
+from wordcloud import WordCloud
+import pandas as pd
+from collections import Counter
+import emoji 
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 st.sidebar.title("Whatsapp Chat Analyser")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
